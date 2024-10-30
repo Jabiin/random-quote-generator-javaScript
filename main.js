68c214f062bf3be837f0f12b6efc95d721b9b3a4
Abdu-rahman <jabiin626@gmail.com>
2024-10-31 02:58:30 +0530

    api url update (#3)
    Co-authored-by: jabiin <jabiin626.com>

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-let quote = document.querySelector('span');
-let person = document.querySelector('strong');
-let btn = document.querySelector('button');
+let quote = document.querySelector("span");
+let person = document.querySelector("strong");
+let btn = document.querySelector("button");
 
+const apiUrl = `https://quotes-api-self.vercel.app/quote`;
 
-const apiUrl = `https://api.quotable.io/random`;
+btn.addEventListener("click", getQuote);
 
-btn.addEventListener('click', getQuote);
+async function getQuote() {
+  const data = await fetch(apiUrl);
 
-
-async function getQuote(){
-  const data = await fetch(apiUrl)
-  const result = await data.json()
-  quote.innerText = result.content
-  person.innerText = result.author
-  
-} 
-
-
+  const result = await data.json();
+  quote.innerText = result.quote;
+  person.innerText = result.author;
+}
